f2b8cabd3ba5e9e391db479c3c8e75061c7245cc
Irina Grigoryan <irinagrigoryan@Irinas-iMac.local>
2021-09-15 16:09:32 -0400

    rev str

--- src/com/company/GitDay2.java
+++ src/com/company/GitDay2.java
@@ -4,4 +4,7 @@ public class GitDay2 {
     public static void main(String [] args){
         System.out.println("Good morning! It is day 2");
     }
+    public static String revStr(String str){
+        return new StringBuilder(str).reverse().toString()
+    }
 }
